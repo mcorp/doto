79c481cfc1d2842dbb95d68f319df86795042042
Leonardo Saraiva <vyper@maneh.org>
2014-04-11 19:01:18 +0000

    Setted correct app name and remove useless task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,6 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'doto'
+set :application, 'doto.mcorp.ws'
 set :repo_url, 'git@github.com:mcorp/doto.git'
 
-namespace :ruby do
-  desc "lala"
-  task :vai do
-    on roles(:app, :web) do
-      as :root do
-        execute "gem update --system"
-      end
-    end
-  end
-end
