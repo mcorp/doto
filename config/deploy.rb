8976ccee980a7c8a92fe2765828c75f47e91e638
Leonardo Saraiva <vyper@maneh.org>
2014-04-13 17:01:33 +0000

    Configured linked dirs and files

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,6 @@ set :application, 'doto.mcorp.ws'
 set :repo_url, 'git@github.com:mcorp/doto.git'
 
 set :deploy_to, "/var/www/#{fetch :application}"
+
+set :linked_dirs, %w{bin log tmp/backup tmp/pids tmp/cache tmp/sockets public}
+set :linked_files, %w{config/database.yml config/unicorn.rb}
