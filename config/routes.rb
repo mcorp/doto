9a7aef45c8965a3fa4c89814c22c458582522101
Leonardo Saraiva <vyper@maneh.org>
2014-04-14 22:30:21 +0000

    Removed batman.js

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,5 @@ Todo::Application.routes.draw do
     end
   end
 
-  get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
-
   resources :tasks, only: [:index, :show, :destroy, :create, :update]
 end
